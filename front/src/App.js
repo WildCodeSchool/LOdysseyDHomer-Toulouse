ecd0bfecaf48e58133bf701d41418c751a8a5dfe
Yassine Tissafi <yassine.tissafi@free.fr>
2018-05-16 16:09:13 +0200

    affichage d'un nouveau composant

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import SignUp from './SignUp'
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -10,12 +12,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SignUp />
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ front/src/SignUp.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="left">
+        <h1>test@test.com</h1>
+        <input type="email" name="email"/>
+      </div>
+    )
+  }
+}
+
+export default SignUp
