49d1bbfa02c30b0b360225b50c7540ecd49376c7
Yassine Tissafi <yassine.tissafi@free.fr>
2018-05-16 17:27:39 +0200

    adresse modifiée en fonction de l'input

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react'
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {email: "test@test.com"}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({email: e.target.value})
+  }
+
   render() {
     return (
       <div className="left">
-        <h1>test@test.com</h1>
-        <input type="email" name="email"/>
+        <input id="textInput" type="email" name="email" onChange={this.handleChange} value={this.state.email}/>
+        <label htmlFor="textInput">
+          <h1>
+            <p dangerouslySetInnerHTML={{__html: this.state.email}} />
+          </h1>
+        </label>
       </div>
     )
   }
