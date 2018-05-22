924ae33297a59a1f53a6ecc79fc0d937eae4fb2f
Romain Vaur <romainwcs@gmail.com>
2018-05-22 16:47:35 +0200

    construction formulaire inscription

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -3,26 +3,49 @@ import React from 'react';
 class SignUp extends React.Component {
   constructor(props){
     super(props)
-    this.updateEmailField = this.updateEmailField.bind(this)
+    this.updateField = this.updateField.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
-      email: ""
+      email: "",
+      password: "",
+      name:"",
+      lastname:""
     }
   }
-  updateEmailField(event){
+  updateField(event){
+    const name = event.target.name
     this.setState({
-      email: event.target.value
+      [name]: event.target.value
     })
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    console.dir(this.state)
   }
   
   render() {
-    return( <div> 
-              <h1>{this.state.email}</h1>
-              <input type="email" name="email" onChange={this.updateEmailField} />
-            </div>);
+    return( <div>
+  <h1>{JSON.stringify(this.state,1,1)}</h1>
+    <form onSubmit={this.handleSubmit}>
+      <div className="form-group">
+        <input type="email" name="email" onChange={this.updateField} placeholder="E-mail"/>
+      </div>
+      <div className="form-group">
+        <input type="password" name="password" onChange={this.updateField} placeholder="Mot de passe"/>
+      </div>
+      <div className="form-group">
+        <input type="password" name="password-confirmation"  placeholder="Confirmation du mot de passe"/>
+      </div>
+      <div className="form-group">
+        <input type="name" name="name" onChange={this.updateField} placeholder="Prénom"/>
+      </div>
+      <div className="form-group">
+        <input type="lastname" name="lastname" onChange={this.updateField} placeholder="Nom"/>
+      </div>
+      <input type="submit" value="Submit" className="btn btn-primary" />
+    </form>
+    </div>);
   }
 }
-
-
-
 
 export default SignUp;
