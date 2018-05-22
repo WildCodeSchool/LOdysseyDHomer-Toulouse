40901b34fa43313ab4106c2d5b545653c483f8d4
Delannel <thomas.delannel@gmail.com>
2018-05-22 14:36:55 +0200

    izi

---
+++ front/src/SignUp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+  constructor(props){
+      super(props)
+      this.updateEmailField = this.updateEmailField.bind(this)
+      this.state = { email: ''}
+    }
+    updateEmailField(event) {
+    this.setState({ email: event.target.value})
+    }
+    render() {
+
+      return (
+        <div>
+          <h1>{this.state.email}</h1>
+          <input type="email" name="email" value={this.state.email} onChange={this.updateEmailField}/>
+        </div>
+      )
+    }
+  }
+
+export default SignUp;
