3f10a7faa20e8781e06585dab524c6755a955b94
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-05-22 10:24:55 +0200

    fin de l'exo

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import SignUp from './SignUp.js'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -13,9 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <SignUp/>
       </div>
     );
   }
 }
 
-export default App;
+export default App
