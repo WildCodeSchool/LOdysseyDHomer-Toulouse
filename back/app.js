8afa2ec3202f244b16a160f7b41e4ca75a7cec92
KhanaeLeff <scenesdecrime@gmail.com>
2018-05-29 09:02:35 +0200

    mise en place de la route

--- back/app.js
+++ back/app.js
@@ -1,28 +1,30 @@
 //je déclare l'ensemble des librairies nécessaires
-const  http  =  require('http');
-const  path  =  require('path');
-const  express  =  require('express');
-const  bodyParser  =  require('body-parser');
+const  http  =  require('http')
+const  path  =  require('path')
+const  express  =  require('express')
+const  bodyParser  =  require('body-parser')
 const  morgan  =  require('morgan');
+const  authRouter = require ('./routes/auth/auth')
 const  app  =  express();
-const  authRouter = ('./routes/auth/auth.js')
 
 //je configure l'application
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended:  false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname  +  '/public'));
-app.use('auth', authRouter);
+
 
 //j'implémentation la partie API
 app.get("/", (req,res)=>{
     res.send("youhou");
 })
+app.use('/auth', authRouter);
+
 /// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
 app.use(function(req, res, next) {
-    var  err  =  new  Error('Not Found');
-    err.status  =  404;
-    next(err);
+    const  err  =  new  Error('Not Found')
+    err.status  =  404
+    next(err)
 });
 
 //je lance le serveur node
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,8 @@
-const  express  =  require('express')
+const express  =  require('express')
 const router = express.Router()
 
-router.post('/signup', function(req,res,next){
-  res.send('I am in POST signup')
+router.post('/signup', function(req, res, next){
+  res.send('I am in POST signup');
 })
 
-module.exports = router
+module.exports = router;
