66d2b312dcc9c2a3225bfbd1c7916e08fa4563e9
Yvan De paep <depaep@hotmail.fr>
2018-05-29 10:32:44 +0200

    quete 020

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,23 +1,50 @@
 import React from 'react'
 
 class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {email: 'test@test.com' };
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      passwordBis: '',
+      name: '',
+      lastname: ''
+    }
 
-    this.updateEmailField = this.updateEmailField.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  handleChange (event) {
+    const name = event.target.name
+    this.setState({
+      [name] : event.target.value
+    })
+       
   }
 
-  updateEmailField(event) {
-    this.setState({email: event.target.value});
+  handleSubmit (event) {
+    event.preventDefault()
+    console.log('A new account has been created:', this.state)
   }
 
   render () {
     return (
-    <div>
-      <h1>{this.state.email}</h1>
-      <input type="email" name="email" value={this.state.value} onChange={this.updateEmailField}/>
-      </div>)
+      <form onSubmit={this.handleSubmit}>
+
+        <input type="email" name="email" placeholder="email" onChange={this.handleChange} /> <br/>
+        <input type="password" name="password" placeholder="password" onChange={this.handleChange} /> <br/>
+        <input type="passwordBis" name="passwordBis" placeholder="password" onChange={this.handleChange} /> <br/>
+        <input type="name" name="name" placeholder="name" onChange={this.handleChange} /> <br/>
+        <input type="lastname" name="lastname" placeholder="lastname" onChange={this.handleChange} /> <br/>
+
+        <input type="submit" value="Soumettre" />
+        <h1>
+          <p dangerouslySetInnerHTML={{__html: JSON.stringify(this.state, 1, 1)}} />
+        </h1>
+
+      </form>
+    )
   }
 }
 export default SignUp
