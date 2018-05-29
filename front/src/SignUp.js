92087dd1b39a847baef60773f6d33ed346038a2d
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-05-29 15:30:21 +0200

    quete 0020

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,24 +1,62 @@
-  import React, { Component } from 'react';
+// eslint-disable-next-line 
+ import React, { Component } from 'react';
   import logo from './logo.svg';
   import './App.css';
+
 
   class SignUp extends Component {
     constructor(props) {
       super(props);
-      this.state = {email: 'test@test.com'};
-      this.updateEmailField= this.updateEmailField.bind(this);
-    }
-    updateEmailField(event) {
-      this.setState({email: event.target.value});
-    }
-    render() {
-      return (
-        <div>
-          <h1>{this.state.email}</h1>
-          <input type="email" name="email" value={this.state.value} onChange={this.updateEmailField} />
-        </div>
-      );
-    }
-}
+      this.state = {
+        email: "mon@email.com",
+        password: "monPassw0rd",
+        passwordBis : "samePassw0rd",
+        firstname: "John",
+        lastname: "Doe"};
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleChange(event) {
+        const name = event.target.name
+        this.setState({
+        [name] : event.target.value
+      })
+      }
+
+
+      handleSubmit(event) {
+        console.log(this.state)
+        event.preventDefault()
+      }
+    
+      render() {
+        const myJSON = JSON.stringify(this.state,1,1)
+        return (
+          <div>
+          <h1>{myJSON}</h1>
+          <form onSubmit={this.handleSubmit}>
+          <label>Email:
+              <input type="email" name="email" onChange={this.handleChange} /> < br/>
+          </label>
+          <label>password:
+              <input type="password" name="password" onChange={this.handleChange} /> < br/>
+          </label>
+          <label>passwordBis:
+              <input type="password" name="password" onChange={this.handleChange} /> < br/>
+          </label>
+          <label>firstname:
+              <input type="firstname" name="firstname" onChange={this.handleChange} /> < br/>
+          </label>
+          <label>lastname:
+              <input type="lastname" name="lastname" onChange={this.handleChange} /> < br />
+          </label>
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
+        )
+      }
+    }
+    
   export default SignUp
