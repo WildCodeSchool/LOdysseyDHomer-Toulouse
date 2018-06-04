dc856eccea092895e107460e903cc49f5f56e0e8
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-06-04 12:01:14 +0200

    quete 0030

---
+++ back/routes/auth/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.post('/signup', function(req, res) {
+  res.send('I am in POST signup')
+})
+
+module.exports = router
+
