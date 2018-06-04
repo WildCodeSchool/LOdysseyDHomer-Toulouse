280d801ff16e7be148f1d77d23ee44ac0cb9d055
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-06-04 16:46:59 +0200

    quete 0040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('../../helpers/db.js')
 
+router.post('/signup', function(req, res, next) {
+  
+  const requetes = `INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)`
 
-router.post('/signup', function(req, res) {
-  res.send('I am in POST signup')
+  const { email, password, name, lastname } = req.body
+  console.log(req.body)
+  connection.query(requetes, [email, password, name, lastname], (error, results) => {
+      if(error) {
+        return res.status(500).json({
+          error: error.message
+        })
+      }
+      res.json(results)
+    })
 })
 
-module.exports = router
+
+module.exports = router 
+
 
