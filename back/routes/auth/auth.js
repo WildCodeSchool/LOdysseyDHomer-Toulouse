2d64fe317c9c17bfcd6d507d7cd8675926cd6000
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-06-04 14:13:54 +0200

    end quete 040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,21 @@
 
 const express = require('express')
 const router = express.Router()
+const connection = require('../../helpers/db.js')
 
-router.post('/signup', function (req, res, next) {
-  res.send('I am in POST signup')
+router.post('/signup', function (req, res) {
+  // res.send('I am in POST signup')
+
+
+  const user = 'INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)'
+  const { email, password, name, lastname } = req.body
+
+  connection.query(user, [email, password, name, lastname], (error, result) => {
+    if(error) {
+      return res.status(500).send('Internal Server Error')
+    }
+    res.json(result)
+  })
 })
+
 module.exports = router
--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "extends": ["standard", "plugin:react/recommended"]
+    "extends": "standard"
 };
