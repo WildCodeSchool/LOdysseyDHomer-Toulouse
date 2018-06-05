22ae0dc01da6a725f67a7acca78a29dce56518e9
Vanthika YOS <vanthika.yos@gmail.com>
2018-06-05 11:08:53 +0200

    add back signup

--- back/app.js
+++ back/app.js
@@ -23,6 +23,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-let  server  =  app.listen( process.env.PORT  ||  3000, function(){
+let  server  =  app.listen( process.env.PORT  ||  3001, function(){
     console.log('Listening on port '  +  server.address().port);
 });
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,24 @@
 const  express  = require('express');
 const router = express.Router()
+const connection = require('../../helpers/db.js')
 
 router.post('/signup', function(req, res) {
-    res.send('I am in POST signup');
+    console.log(req.body)
+    const email = req.body.email
+    const password = req.body.password
+    const name = req.body.name
+    const lastname = req.body.lastname
+    
+    // const query = `INSERT INTO users(email, password, name, lastname) VALUES ('${email}', '${password}', '${name}', '${lastname}')`
+    const query = `INSERT INTO users(email, password, name, lastname) VALUES (?,?,?,?)`
+    connection.query(query, [email, password, name, lastname], function(error, results, fields) {
+        if(error) {
+            console.error(error)
+            return res.status(500).end();
+        }
+        res.send('I am in POST signup');
+    });
+    // res.send(req.body);
 });
 
 module.exports = router;
